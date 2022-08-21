991e6e8feea33558a7202c914cf949da5a7cc2a8
Lars Richter <github@n-develop.com>
2022-08-21 16:02:12 +0200

    Initial Commit

---
+++ LearnA11y/LearnA11yApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnA11yApp.swift
+//  LearnA11y
+//
+//  Created by Lars Richter on 21.08.22.
+//
+
+import SwiftUI
+
+@main
+struct LearnA11yApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
